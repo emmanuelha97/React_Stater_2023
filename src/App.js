eac65dfdba37357bce32b1f8c2e6950f2b97b757
emmanuelhernandez <emmanuelha97@gmail.com>
2022-11-22 23:06:14 -0500

    Added button and useState simple example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import AnimalShow from './AnimalShow'
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div>
-      <div>Show the animal lsit</div>
-      <AnimalShow />
-      <AnimalShow />
-      <AnimalShow />
+      <button onClick={handleClick}>Add animal</button>
+      <div>Number of animals : {count}</div>
     </div>
   );
 }
 
-export default AnimalShow
+export default App
