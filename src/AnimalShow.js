0dffd82796c5598fae7febeeb5c9bf8eb88f712a
emmanuelhernandez <emmanuelha97@gmail.com>
2022-11-23 23:10:15 -0500

    Animal app complete Styling was optional so I skipped

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,34 @@
-function AnimalShow(){
+import {useState} from "react";
+import bird from './images/svg/bird.svg'
+import cat from './images/svg/cat.svg'
+import cow from './images/svg/cow.svg'
+import dog from './images/svg/dog.svg'
+import gator from './images/svg/gator.svg'
+import heart from './images/svg/heart.svg'
+import horse from './images/svg/horse.svg'
+
+
+const svgMap = {
+  bird,
+  cat,
+  cow,
+  dog,
+  gator,
+  horse,
+  heart
+}
+
+function AnimalShow({type}){
+  const [clicks, setClicks] = useState(0)
+
+  const handleClick = () => {
+    setClicks(clicks + 1)
+  }
+
   return (
-    <div>
-     I am an animal
+    <div onClick={handleClick}>
+      <img src={svgMap[type]} alt="animal" style={{width: 50 + 'px', height: 'auto'}}/>
+      <img src={svgMap['heart']} alt="heart" style={{ width: 10 + 10 * clicks + 'px'}}/>
     </div>
   )
 }
--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import AnimalShow from './AnimalShow'
 import { useState } from 'react'
 
+function getRandomAnimal(){
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [animals, setAnimals] = useState([])
+
 
   const handleClick = () => {
-    setCount(count + 1)
+    setAnimals([...animals, getRandomAnimal()])
   }
+
+  const renderedAnimals = animals.map((animal, index) => {
+    return (
+      <AnimalShow type={animal} key={index}/>
+    )
+  })
 
   return (
     <div>
       <button onClick={handleClick}>Add animal</button>
-      <div>Number of animals : {count}</div>
+      {renderedAnimals}
     </div>
   );
 }
