e1ec30141298fcda995a841b0224c2b46386fb90
emmanuelhernandez <emmanuelha97@gmail.com>
2022-11-24 14:22:26 -0500

    Final commit Css is optional, so I am skipping I set up flask server in the back, so I would not overload the API. Replace the url in the axios get with the unsplash_url. Removed Client-ID need to get that from the unsplash website

--- src/api.js
+++ src/api.js
@@ -4,9 +4,6 @@ const unsplash_url = 'https://api.unsplash.com/search/photos'
 
 const searchImages = async (term) => {
   const response = await axios.get('http://127.0.0.1:5000/alldata', {
-    headers: {
-      Authorization: 'Client-ID GOBIPAS7c_qQSxpLnOewUxS4-5nEJup-dtg_roml7tc'
-    },
     params: {
       query: term
     }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,12 @@
-function ImageList() {
+import ImageShow from "./ImageShow";
+
+function ImageList({images}) {
+  const renderedImages = images.map((image, index) => {
+    return <ImageShow key={image.id} image={image}/>;
+  })
   return (
     <div>
-      ImageList
+      {renderedImages}
     </div>
   );
 }
